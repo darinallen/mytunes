f0a7487ea7458537b6fdee88c4562c0484f8c669
Darin Allen <darinallen@gmail.com>
2017-02-23 01:25:16 -0500

    Partial solution for SongQueue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,32 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+
+    // this.on('dequeue', function() {
+    //   this.removeSong
+    // });
+
+    this.on('add', function(e) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function(e) {
+      this.removeFirst();
+    });
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  removeFirst: function() {
+    this.remove(this.at(0));
   }
 
+  // removeSong: function() {
+  //   this.remove();
+  // }
 });
