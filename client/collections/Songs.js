8e80bf6bd84d50a519ebeaa08f0c5ae8fe821ce4
Darin Allen <darinallen@gmail.com>
2017-02-23 18:40:02 -0500

    All tests passing

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -3,30 +3,14 @@ var Songs = Backbone.Collection.extend({
 
   model: SongModel,
 
-  server: 'http://parse.hrr.hackreactor.com/mytunes/classes/songs',
+  url: 'http://parse.hrr.hackreactor.com/mytunes/classes/songs',
+
+  parse: function(response) {
+    return response.results;
+  },
 
   initialize: function() {
-    var context = this;
-    $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-      url: this.server,
-      type: 'GET',
-      contentType: 'application/json',
-
-      success: function(songs) {
-        console.log('arguments: ', arguments);
-        console.log('songs received');
-        var songs = songs.results;
-        console.log('songs: ', songs);
-        songs.forEach(function(song) {
-          context.add(song);
-        });
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('MyTunes: Failed to receive songs', data);
-      }
-    });
-  }
+    this.fetch();
+  },
 
 });
